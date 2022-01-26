c6e1b504c913f80607245d8cf17ee9d465043d80
baroobarecked <aaron@brubecksecure.com>
2022-01-26 11:50:58 -0600

    added splash page

--- frontend/src/components/UserPages/Splash.js
+++ frontend/src/components/UserPages/Splash.js
@@ -3,6 +3,23 @@
 
 export default function SplashPage () {
     return (
-        <h1 id="test">Hello World</h1>
+        <div id='splashContent'>
+            <div id='splashTitle'></div>
+            <div id='introduction'>
+                <h1>Welcome to Digital Eclipse</h1>
+                <h2>The best digital way to listen to analog audio</h2>
+                <div id="intro_grid">
+                    <h3 id='grid_title'>Get started with a few simple steps</h3>
+                    <h4 id='step_one'>1. Record your audio.</h4>
+                    <img id='img_one' src='https://bucketeer-c8e3fd63-1c26-4660-8f22-707352f21248.s3.amazonaws.com/3ce994bc-28a5-4883-961b-264c0de1dd8brecord.png'></img>
+                    <h4 id='step_two'>2. Create a profile.</h4>
+                    <img id='img_two' src='https://bucketeer-c8e3fd63-1c26-4660-8f22-707352f21248.s3.amazonaws.com/f4ed97de-0b02-4afc-b9f1-85729e6db281profile.png'></img>
+                    <h4 id='step_three'>3. Upload the audio.</h4>
+                    <img id='img_three' src='https://bucketeer-c8e3fd63-1c26-4660-8f22-707352f21248.s3.amazonaws.com/75265237-5eeb-4958-8a06-e860c6268624uploading.png'></img>
+                    <h4 id='step_four'>4. Enjoy.</h4>
+                    <img id='img_four' src='https://bucketeer-c8e3fd63-1c26-4660-8f22-707352f21248.s3.amazonaws.com/15661cc6-271d-42eb-a7a9-dd5f0766e046streaming.png'></img>
+                </div>
+            </div>
+        </div>
     )
 }
