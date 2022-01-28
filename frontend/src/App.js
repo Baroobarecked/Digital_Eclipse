c78bf20d58f74fe2f65a19b9b64d956122617204
baroobarecked <aaron@brubecksecure.com>
2022-01-28 11:22:43 -0600

    added default re-routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import SignUp from './components/UserPages/Signup';
 import Login from './components/UserPages/Login';
 import AddAlbum from './components/AlbumsPage/addnew';
@@ -40,21 +40,24 @@ function App() {
           <Route index element={<SplashPage />} />
           <Route exact path='signup' element={<SignUp />} />
           <Route exact path='login' element={<Login />} />
+          <Route path='*' element={<Navigate to='/login' replace />} />
         </Route>
         <Route exact path='albums' element={<Albums />}>
           <Route exact path='addalbum' element={<AddAlbum />} />
-          <Route exact path=':albumId' element={<AddAlbum />} />
-          <Route exact path=':albumId/songs' element={<SongForm />} />
+          <Route exact path=':albumId(\\d+)' element={<AddAlbum />} />
+          <Route exact path=':albumId(\\d+)/songs' element={<SongForm />} />
+          <Route path='*' element={<Navigate to='' replace />} />  
         </Route>
         <Route exact path='community' element={<Albums />}>
-            <Route path='' element={<ForumPage />}>
-              <Route path=':forumid/:forumtitle' element={<Posts />} >
-              </Route>
+          <Route path='' element={<ForumPage />}>
+            <Route path=':forumid(\\d+)/:forumtitle' element={<Posts />} >
             </Route>
+          </Route>
           <Route exact path='addforumdisscussion' element={<CreateForum />} />
+          <Route path='*' element={<Navigate to='' replace />} />
         </Route>
         <Route exact path='vr/albums' element={<VrMain />}>
-
+        <Route path='*' element={<Navigate to='/' replace />} />
         </Route>
       </Routes>
     </BrowserRouter>
