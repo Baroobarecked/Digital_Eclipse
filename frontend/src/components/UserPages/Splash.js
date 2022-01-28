25b16ff84dcbc26aca5cde0881e5b3058de34445
baroobarecked <aaron@brubecksecure.com>
2022-01-28 10:55:35 -0600

    added about links

--- frontend/src/components/UserPages/Splash.js
+++ frontend/src/components/UserPages/Splash.js
@@ -20,6 +20,14 @@ export default function SplashPage () {
                     <img id='img_four' src='https://bucketeer-c8e3fd63-1c26-4660-8f22-707352f21248.s3.amazonaws.com/15661cc6-271d-42eb-a7a9-dd5f0766e046streaming.png'></img>
                 </div>
             </div>
+            <div id='about_links'>
+                <a href="https://github.com/Baroobarecked">
+                    <img src="https://bl3302files.storage.live.com/y4mO_0V5dUEBoWRdPr7mlFOZesVVqZ2HUEsfYGFFngJaRZxwuKx3748YIBXKkMs_0BW0Dwwf8i7aqS09bV0tcgn2nBAX3_eF-A3CRl5CY4mOrERyc3EV5oH7nmfHz7sR0w2NfSI-sbrGi_hMUOPi8J4wrcFpFwaFxb8Mij_upad2cEor4U4qZ8s9JywEadZq0mT?width=512&height=512&cropmode=none" />
+                </a>
+                <a href="https://linkedin.com/in/aaron-brubeck-458890183">
+                    <img src="https://bl3302files.storage.live.com/y4mL_-zfos5cPG5c1Lc9nvwfOjkgyncFc5Y6gKamCz0tKAGiu9zvO0__6jGcdEwUrH0S06B0ekEAggD9FlfjDdOYKvYtzEZyqfz5OPCOFJ1UNhghrq8Rjh8HATe1s1ki_LvXB7b7qTJeywfyfJOxkt8jSiEdcehUDU-LgZUW-Nt8DbjpEgR2jESwa3rIuXzza2k?width=600&height=600&cropmode=none" />
+                </a>
+            </div>
         </div>
     )
 }
